513478cbc44b5b512cffa156cd49ddd23e147abf
Ben Hopkins <benhopkinstech@gmail.com>
2023-02-21 20:09:28 +0000

    Added jwt and sendgrid package

--- IdentityPostgres/Program.cs
+++ IdentityPostgres/Program.cs
@@ -7,11 +7,17 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<IdentityContext>(options =>
 options.UseNpgsql(builder.Configuration.GetConnectionString("Database")));
 
+builder.Services.AddAuthentication().AddJwtBearer();
+builder.Services.AddAuthorization();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.RegisterModules();
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 if (app.Environment.IsDevelopment())
 {
